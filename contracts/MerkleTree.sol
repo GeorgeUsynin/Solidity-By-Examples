fde4e0f2dd2c26b660ae51d5dccaa1fb6e60806f
George Usynin <angiejo03@gmail.com>
2025-07-03 19:56:44 +0300

    feat: :sparkles: add merkle tree contract

---
+++ contracts/MerkleTree.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract MerkleTree {
+    bytes32[] public hashes;
+
+    constructor(string[] memory transactionsArr){
+        for(uint i = 0; i < transactionsArr.length; i++){
+            hashes.push(makeHash(transactionsArr[i]));
+        }
+
+        uint count = transactionsArr.length;
+        uint offset = 0;
+
+        while(count > 1) {
+            for(uint i = 0; i < count; i += 2){
+                hashes.push(keccak256(
+                    abi.encodePacked(hashes[offset + i], hashes[offset + i + 1])
+                    ));
+            }
+
+            offset += count;
+            count = count / 2;
+        }
+    }
+
+    function verify(string memory transaction, uint index, bytes32 root, bytes32[] memory proof) public pure returns (bool) {
+        bytes32 hash = makeHash(transaction);
+
+        for(uint i = 0; i < proof.length; i++){
+            bytes32 element = proof[i];
+            if(index % 2 == 0){
+                hash = keccak256(abi.encodePacked(hash, element));
+            } else {
+                hash = keccak256(abi.encodePacked(element, hash));
+            }
+
+            index = index / 2;
+        }
+
+        return hash == root;
+    }
+
+    function makeHash(string memory input) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(input));
+    }
+}
