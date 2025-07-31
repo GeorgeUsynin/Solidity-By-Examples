4033e736f24e400bccce49d64775ce3ff092fca2
George Usynin <angiejo03@gmail.com>
2025-07-31 13:55:44 +0300

    feat: :zap: add crowdfunding contract + tests

---
+++ contracts/Crowdfunding.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+error GoalShouldBeMoreThanZero();
+error EndAtShouldBeBetween3And30Days();
+error CampaignAlreadyEnded();
+error CampaignStillInProgress();
+error ZeroPledgeAmount();
+error InsufficientAmount();
+error NotAnOrganizer();
+error GoalWasNotReached();
+error GoalAlreadyClaimed();
+error GoalWasReached();
+error WrongContractAddress();
+error NothingToRefund();
+
+contract LowkickStarter {
+    struct LowkickCampaign {
+        Campaign targetContract;
+        bool claimed;
+    }
+    mapping(uint => LowkickCampaign) public campaigns;
+    uint private currentCampaignId;
+    address owner;
+    uint constant MIN_DURATION = 3 days;
+    uint constant MAX_DURATION = 30 days;
+
+    event CampaignStarted(uint id, uint goal, uint endsAt, address organizer);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function startCampaign(uint _goal, uint _endsAt) public {
+        require(_goal > 0, GoalShouldBeMoreThanZero());
+        require(
+            _endsAt >= block.timestamp + MIN_DURATION &&
+                _endsAt <= block.timestamp + MAX_DURATION,
+            EndAtShouldBeBetween3And30Days()
+        );
+
+        currentCampaignId += 1;
+
+        // contract creation
+        Campaign newCampaign = new Campaign(
+            _endsAt,
+            _goal,
+            msg.sender,
+            currentCampaignId
+        );
+
+        campaigns[currentCampaignId] = LowkickCampaign({
+            targetContract: newCampaign,
+            claimed: false
+        });
+
+        emit CampaignStarted(currentCampaignId, _goal, _endsAt, msg.sender);
+    }
+
+    function onClaimed(uint id) external {
+        LowkickCampaign storage targetCampaign = campaigns[id];
+        require(
+            msg.sender == address(targetCampaign.targetContract),
+            WrongContractAddress()
+        );
+
+        targetCampaign.claimed = true;
+    }
+}
+
+contract Campaign {
+    uint public endsAt;
+    uint public goal;
+    uint public id;
+    uint public pledged;
+    address public organizer;
+    LowkickStarter parent;
+    bool claimed;
+    mapping(address => uint) public pledges;
+
+    event Pledged(uint amount, address pledger);
+
+    constructor(uint _endsAt, uint _goal, address _organizer, uint _id) {
+        endsAt = _endsAt;
+        goal = _goal;
+        organizer = _organizer;
+        // msg.sender will be LowkickStarter contract since he is creating Campaign contract using new keyword
+        parent = LowkickStarter(msg.sender);
+        id = _id;
+    }
+
+    function pledge() external payable {
+        require(block.timestamp <= endsAt, CampaignAlreadyEnded());
+        require(msg.value > 0, ZeroPledgeAmount());
+
+        pledged += msg.value;
+        pledges[msg.sender] += msg.value;
+
+        emit Pledged(msg.value, msg.sender);
+    }
+
+    function refundPledge(uint _amount) external {
+        require(block.timestamp <= endsAt, CampaignAlreadyEnded());
+        require(_amount <= pledges[msg.sender], InsufficientAmount());
+
+        pledges[msg.sender] -= _amount;
+        pledged -= _amount;
+
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function claim() external {
+        require(block.timestamp > endsAt, CampaignStillInProgress());
+        require(msg.sender == organizer, NotAnOrganizer());
+        require(pledged >= goal, GoalWasNotReached());
+        require(!claimed, GoalAlreadyClaimed());
+
+        claimed = true;
+        payable(organizer).transfer(pledged);
+
+        parent.onClaimed(id);
+    }
+
+    function fullRefund() external {
+        require(block.timestamp > endsAt, CampaignStillInProgress());
+        require(pledged < goal, GoalWasReached());
+
+        uint refundAmount = pledges[msg.sender];
+        require(refundAmount > 0, NothingToRefund());
+
+        pledges[msg.sender] = 0;
+        payable(msg.sender).transfer(refundAmount);
+    }
+}
---
+++ test/Crowdfunding.test.ts
@@ -0,0 +1,102 @@
+import { loadFixture, ethers, expect, time, mine } from './setup';
+import { Campaign__factory } from '../typechain-types';
+
+describe('Crowdfunding', function () {
+  async function deploy() {
+    const [owner, organizer, pledger] = await ethers.getSigners();
+
+    const Factory = await ethers.getContractFactory('LowkickStarter');
+    const lowkickStarter = await Factory.deploy();
+    await lowkickStarter.waitForDeployment();
+
+    return { lowkickStarter, owner, organizer, pledger };
+  }
+
+  it('allows to pledge and claim', async function () {
+    const { lowkickStarter, organizer, pledger } = await loadFixture(deploy);
+
+    const goal = ethers.parseEther('2.0');
+    const endsAt = Math.floor(Date.now() / 1000) + 259205;
+
+    // creating campaign
+    await lowkickStarter.connect(organizer).startCampaign(goal, endsAt);
+    const campaignContract = (await lowkickStarter.campaigns(1)).targetContract;
+
+    const campaignAsPledger = Campaign__factory.connect(
+      campaignContract,
+      pledger,
+    );
+    const campaignAsOrganizer = Campaign__factory.connect(
+      campaignContract,
+      organizer,
+    );
+
+    const pledgeTx = await campaignAsPledger.pledge({
+      value: ethers.parseEther('2.5'),
+    });
+    await pledgeTx.wait();
+
+    await expect(campaignAsOrganizer.claim()).to.be.revertedWithCustomError(
+      campaignAsOrganizer,
+      'CampaignStillInProgress',
+    );
+
+    await time.increase(259210);
+
+    const claimTx = await campaignAsOrganizer.claim();
+    await claimTx.wait();
+
+    await expect(claimTx).to.changeEtherBalances(
+      [campaignContract, organizer],
+      [ethers.parseEther('-2.5'), ethers.parseEther('2.5')],
+    );
+    await expect(campaignAsOrganizer.claim()).to.be.revertedWithCustomError(
+      campaignAsOrganizer,
+      'GoalAlreadyClaimed',
+    );
+  });
+
+  it('allows full refund if campaign does not reach the goal', async function () {
+    const { lowkickStarter, organizer, pledger } = await loadFixture(deploy);
+
+    const goal = ethers.parseEther('2.0');
+    const endsAt = Math.floor(Date.now() / 1000) + 259205;
+
+    // creating campaign
+    await lowkickStarter.connect(organizer).startCampaign(goal, endsAt);
+    const campaignContract = (await lowkickStarter.campaigns(1)).targetContract;
+
+    const campaignAsPledger = Campaign__factory.connect(
+      campaignContract,
+      pledger,
+    );
+    const campaignAsOrganizer = Campaign__factory.connect(
+      campaignContract,
+      organizer,
+    );
+
+    const pledgeTx = await campaignAsPledger.pledge({
+      value: ethers.parseEther('1.0'),
+    });
+    await pledgeTx.wait();
+
+    await time.increase(259210);
+
+    await expect(campaignAsOrganizer.claim()).to.be.revertedWithCustomError(
+      campaignAsOrganizer,
+      'GoalWasNotReached',
+    );
+
+    const fullRefundTx = await campaignAsPledger.fullRefund();
+    await fullRefundTx.wait();
+
+    await expect(fullRefundTx).to.changeEtherBalances(
+      [campaignContract, pledger],
+      [ethers.parseEther('-1.0'), ethers.parseEther('1.0')],
+    );
+    await expect(campaignAsPledger.fullRefund()).to.be.revertedWithCustomError(
+      campaignAsPledger,
+      'NothingToRefund',
+    );
+  });
+});
