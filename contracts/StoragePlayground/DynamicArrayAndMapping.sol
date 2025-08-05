62fc838b7bddbcdd69051510d92a24caec20c21d
George Usynin <angiejo03@gmail.com>
2025-08-05 18:46:04 +0300

    feat: :art: add string representaion in storage with tests

--- contracts/StoragePlayground/DynamicArrayAndMapping.sol
+++ contracts/StoragePlayground/DynamicArrayAndMapping.sol
@@ -6,6 +6,8 @@ contract DynamicArrayAndMapping {
     uint a = 100; // slot 0
     uint[] arr; // slot 1
     mapping(address => uint) balances; // slot 2
+    string longStr = 'thisIsVeryLongStringAndWeCanDoThis'; // slot 3
+    string shortStr = 'test'; //slot 4
 
     constructor(uint[2] memory _arr) {
         arr.push(_arr[0]);
--- test/StoragePlayground/DynamicArrayAndMapping.test.ts
+++ test/StoragePlayground/DynamicArrayAndMapping.test.ts
@@ -1,5 +1,7 @@
 import { loadFixture, ethers, expect } from '../setup';
 
+const LONG_STR = 'thisIsVeryLongStringAndWeCanDoThis';
+const SHORT_STR = 'test';
 const ARR = [10n, 20n] as [bigint, bigint];
 
 describe('DynamicArrayAndMapping', function () {
@@ -90,5 +92,76 @@ describe('DynamicArrayAndMapping', function () {
 
       expect(value).to.eq(ethers.toBeHex(expected, 32));
     }
+  });
+
+  it('shows values for string > 31 bytes', async function () {
+    const { dynamicArrayAndMapping } = await loadFixture(deploy);
+
+    /**
+     * If the string is > 31 bytes:
+     * - The storage slot stores only a special "pointer" value: (length * 2) + 1
+     *   - The last bit set to 1 indicates that the string data is stored at a separate location.
+     * - The actual string content starts at the storage slot:
+     *
+     *     keccak256(p)
+     *
+     *   where `p` is the slot number where the string is declared.
+     * - The string occupies as many slots as needed (data is stored consecutively).
+     */
+
+    const lengthPlusBitValue = ethers.toBigInt(LONG_STR.length * 2 + 1);
+    expect(
+      await ethers.provider.getStorage(dynamicArrayAndMapping.target, 3),
+    ).to.eq(ethers.toBeHex(lengthPlusBitValue, 32));
+
+    const pos = ethers.solidityPackedKeccak256(['uint'], [3]);
+    const nexPositionAsInteger = ethers.toBigInt(pos) + 1n;
+
+    const slots = [pos, nexPositionAsInteger];
+    const values = [];
+
+    for (let i = 0; i < slots.length; i++) {
+      const slot = slots[i];
+
+      const value = await ethers.provider.getStorage(
+        dynamicArrayAndMapping.target,
+        slot,
+      );
+
+      if (!value.endsWith('00')) {
+        // ethers.toUtf8String does NOT trim trailing null bytes (`0x00`)
+        // Use this when the string might span multiple storage slots (e.g., long strings)
+        values.push(ethers.toUtf8String(value));
+      } else {
+        // ethers.decodeBytes32String trims trailing null bytes automatically
+        // Use this only when you're certain the value fits into a single bytes32 slot
+        values.push(ethers.decodeBytes32String(value));
+      }
+    }
+
+    expect(values.join('')).to.eq(LONG_STR);
+  });
+
+  it('shows values for string <= 31 bytes', async function () {
+    const { dynamicArrayAndMapping } = await loadFixture(deploy);
+
+    /**
+     * If the string is ≤ 31 bytes:
+     * - The entire string is stored directly in the storage slot.
+     * - The actual string data is stored in the lower-order bytes.
+     * - The last byte (lowest byte) encodes (length * 2).
+     *
+     * Note: The lowest bit is set to 0 to indicate that the data is stored directly in the slot (not in a separate storage location).
+     */
+
+    const valueWithLength = await ethers.provider.getStorage(
+      dynamicArrayAndMapping.target,
+      4,
+    );
+
+    expect(ethers.toUtf8String(valueWithLength.slice(0, 10))).to.eq(SHORT_STR);
+    expect(+valueWithLength[valueWithLength.length - 1]).to.eq(
+      SHORT_STR.length * 2,
+    );
   });
 });
